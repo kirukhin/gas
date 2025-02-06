e6f22c9f5e4a58a7b357ae62a8eb57a29ad5c846
kirukhin <kirukhin@gmail.com>
2025-02-06 07:34:12 +0000

    Updates

--- components/data.js
+++ components/data.js
@@ -1,7 +1,7 @@
 const data = {
     O30C: {
       "model": "O30C",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 28.2677932345,
       "productivityM3H": 1.6960675941,
       "productivityKgH": 2.2614234588,
@@ -9,52 +9,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 110 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 66.4223262695,
           "price": 56000
         },
         "oResiver": {
           "model": "Ресивер кислородный 100 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 28.2677932345,
           "price": 75000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 332 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 332.1116313476,
           "price": 10
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 10
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 10
         },
         "nResiver": {
           "model": "Ресивер азотный 110 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 10
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 10
         }
       }
     },
     O50C: {
       "model": "O50C",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 55.4048747396,
       "productivityM3H": 3.3242924844,
       "productivityKgH": 4.4323899792,
@@ -62,52 +62,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 280 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 130.1877594883,
           "price": 65000
         },
         "oResiver": {
           "model": "Ресивер кислородный 100 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 55.4048747396,
           "price": 72000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 651 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 650.9387974414,
           "price": 10
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 10
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 10
         },
         "nResiver": {
           "model": "Ресивер азотный 110 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 10
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O100: {
       "model": "O100",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 105.5479353335,
       "productivityM3H": 6.33287612,
       "productivityKgH": 8.4438348267,
@@ -115,52 +115,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 280 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 248.0115564609,
           "price": 65000
         },
         "oResiver": {
           "model": "Ресивер кислородный 250 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 105.5479353335,
           "price": 105000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 1250 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 1240.0577823043,
           "price": 10
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 10
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 10
         },
         "nResiver": {
           "model": "Ресивер азотный 280 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 10
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O140: {
       "model": "O140",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 124.6470855367,
       "productivityM3H": 7.4788251322,
       "productivityKgH": 9.9717668429,
@@ -168,52 +168,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 292.8898381062,
           "price": 86000
         },
         "oResiver": {
           "model": "Ресивер кислородный 250 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 124.6470855367,
           "price": 105000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 1465 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 1464.4491905308,
           "price": 10
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 10
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 10
         },
         "nResiver": {
           "model": "Ресивер азотный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 10
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O200: {
       "model": "O200",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 179.9358110547,
       "productivityM3H": 10.7961486633,
       "productivityKgH": 14.3948648844,
@@ -221,52 +221,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 422.804675636,
           "price": 86000
         },
         "oResiver": {
           "model": "Ресивер кислородный 250 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 179.9358110547,
           "price": 105000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 2115 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 2114.0233781802,
           "price": 10
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 10
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 10
         },
         "nResiver": {
           "model": "Ресивер азотный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 10
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O250: {
       "model": "O250",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 250,
       "productivityM3H": 15,
       "productivityKgH": 20,
@@ -274,52 +274,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 500,
           "price": 86000
         },
         "oResiver": {
           "model": "Ресивер кислородный 250 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 250,
           "price": 105000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 3000 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 3000,
           "price": 50
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 50
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 50
         },
         "nResiver": {
           "model": "Ресивер азотный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 50
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O400: {
       "model": "O400",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 400,
       "productivityM3H": 24,
       "productivityKgH": 32,
@@ -327,52 +327,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресивер воздушный 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 800,
           "price": 150000
         },
         "oResiver": {
           "model": "Ресивер кислородный 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 400,
           "price": 120000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 4800 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 4800,
           "price": 75
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 75
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 75
         },
         "nResiver": {
           "model": "Ресивер азотный 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 75
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O600: {
       "model": "O60",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 600,
       "productivityM3H": 36,
       "productivityKgH": 48,
@@ -380,52 +380,52 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресиверы воздушные 900 + 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 1200,
           "price": 236000
         },
         "oResiver": {
           "model": "Ресивер кислородный 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 600,
           "price": 190000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 7200 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 7200,
           "price": 100
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 100
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 100
         },
         "nResiver": {
           "model": "Ресиверы азотные 900 + 500 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 100
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
     },
     O750: {
       "model": "O750",
-      "url": "/gas/adsorber.png",
+      "url": "/adsorber.png",
       "productivityLMin": 750,
       "productivityM3H": 45,
       "productivityKgH": 60,
@@ -433,45 +433,45 @@ const data = {
       "equipment": {
         "vResiver": {
           "model": "Ресиверы воздушные 900 + 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "inputReceiverVolumeL": 1500,
           "price": 295000
         },
         "oResiver": {
           "model": "Ресивер кислородный 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "outputReceiverVolumeL": 750,
           "price": 190000
         },
         "kompressor": {
           "model": "Воздушный компрессор на 9000 л/мин",
-          "url": "/gas/compressor.png",
+          "url": "/compressor.png",
           "airConsumptionLMin": 9000,
           "price": 125
         },
         "osyshitel": {
           "model": "Рефрижираторный осушитель",
-          "url": "/gas/osyshitel.png",
+          "url": "/osyshitel.png",
           "price": 125
         },
         "filtr": {
           "model": "Система фильтрации MSA",
-          "url": "/gas/filtr.png",
+          "url": "/filtr.png",
           "price": 125
         },
         "nResiver": {
           "model": "Ресиверы азотные 900 + 900 л",
-          "url": "/gas/reciver.png",
+          "url": "/reciver.png",
           "price": 125
         },
         "dKompressor": {
           "model": "Дожимной компрессор",
-          "url": "/gas/dcompressor.png",
+          "url": "/dcompressor.png",
           "price": 85000
         },
         "rampa": {
           "model": "Накопительная рампа",
-          "url": "/gas/rampa.png",
+          "url": "/rampa.png",
           "price": 55000
         }
       }
