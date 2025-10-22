f5f6685bf4474f1321152259f2adfd3f74ce6023
kirukhin <kirukhin@gmail.com>
2025-10-22 16:52:39 +0000

    test deploy

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,14 @@
+// next-sitemap.config.cjs
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://blitzgas.ru',
+    changefreq: 'monthly',
+    priority: 0.7,
+    generateRobotsTxt: true,
+    // exclude: ['/secret'],
+    robotsTxtOptions: {
+      policies: [
+        { userAgent: '*', allow: '/' }
+      ]
+    }
+  }
+  
