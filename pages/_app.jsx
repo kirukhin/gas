585809a1deda23e2ff08c14315dfe0ce695f15ac
kirukhin <kirukhin@gmail.com>
2025-10-22 12:23:25 +0000

    fix(sitemap): use CommonJS config (next-sitemap.config.cjs)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -47,6 +47,9 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#d10000" />
+
       </Head>
 
       <Layout>
